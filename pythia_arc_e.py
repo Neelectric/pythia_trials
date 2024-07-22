c516983b03a20144b60ed32bbc1eeee9f04fc73b
Neelectric <Neel.R@web.de>
2024-07-22 20:49:22 +0200

    Adding some code...

--- pythia_arc_e.py
+++ pythia_arc_e.py
@@ -1,4 +1,18 @@
-from transformers import AutoTokenizer, AutoModel
-import torch
+from transformers import GPTNeoXForCausalLM, AutoTokenizer
 
-print("Hello World!")
+model = GPTNeoXForCausalLM.from_pretrained(
+  "EleutherAI/pythia-70m-deduped",
+  revision="step3000",
+  cache_dir="./pythia-70m-deduped/step3000",
+)
+
+tokenizer = AutoTokenizer.from_pretrained(
+  "EleutherAI/pythia-70m-deduped",
+  revision="step3000",
+  cache_dir="./pythia-70m-deduped/step3000",
+)
+
+inputs = tokenizer("Hello, I am", return_tensors="pt")
+tokens = model.generate(**inputs)
+tokenizer.decode(tokens[0])
+print(tokenizer.decode(tokens[0]))
