88adf44a3f1c3cabb844d241ac1533052985cb66
Neelectric <Neel.R@web.de>
2024-07-22 22:24:40 +0200

    Starting to build pythia inference

--- pythia_arc_e.py
+++ pythia_arc_e.py
@@ -1,18 +1,35 @@
-from transformers import GPTNeoXForCausalLM, AutoTokenizer
+import subprocess
+from huggingface_hub import HfApi
 
-model = GPTNeoXForCausalLM.from_pretrained(
-  "EleutherAI/pythia-70m-deduped",
-  revision="step3000",
-  cache_dir="./pythia-70m-deduped/step3000",
-)
+api = HfApi()
+refs = api.list_repo_refs("EleutherAI/pythia-14m")
+revisions = [ref.ref.split('/')[-1] for ref in refs.branches]
+revisions.reverse() 
+# print(revisions)
+revisions.pop(0)
 
-tokenizer = AutoTokenizer.from_pretrained(
-  "EleutherAI/pythia-70m-deduped",
-  revision="step3000",
-  cache_dir="./pythia-70m-deduped/step3000",
-)
+base_command = [
+    "lm_eval",
+    "--model", "hf",
+    "--model_args", "pretrained=EleutherAI/pythia-14m",
+    "--tasks", "arc_easy",
+    "--device", "cuda:0",
+    "--batch_size", "32",
+    "--output_path", "output/pythia_arc",
+    # "--use_cache", "output/pythia_arc"
+]
+for revision in revisions:
+    full_command = base_command.copy()
+    full_command[4] += f",revision={revision},dtype=float"
+    
+    print(f"Running command: {' '.join(full_command)}")
+    
+    # Run the command
+    try:
+        result = subprocess.run(full_command, check=True, text=True, capture_output=True)
+        print(f"Command for revision {revision} completed successfully")
+    except subprocess.CalledProcessError as e:
+        print(f"Error running command for revision {revision}: {e}")
+        print(f"Error output: {e.stderr}")
 
-inputs = tokenizer("Hello, I am", return_tensors="pt")
-tokens = model.generate(**inputs)
-tokenizer.decode(tokens[0])
-print(tokenizer.decode(tokens[0]))
+print("All evaluations completed")
