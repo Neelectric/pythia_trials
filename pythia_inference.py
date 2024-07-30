433c63c207378314b537a6f0aa5a63968d78262a
Neelectric <Neel.R@web.de>
2024-07-30 18:38:53 +0200

    Changing generation to greedy decoding

--- pythia_inference.py
+++ pythia_inference.py
@@ -2,17 +2,22 @@ from transformers import GPTNeoXForCausalLM, AutoTokenizer
 
 model = GPTNeoXForCausalLM.from_pretrained(
   "EleutherAI/pythia-70m-deduped",
-  revision="step3000",
-  cache_dir="./pythia-70m-deduped/step3000",
+  # revision="step3000",
+  # cache_dir="./pythia-70m-deduped/step3000",
+  device_map="cuda:0",
 )
 
 tokenizer = AutoTokenizer.from_pretrained(
   "EleutherAI/pythia-70m-deduped",
-  revision="step3000",
-  cache_dir="./pythia-70m-deduped/step3000",
+  # revision="step3000",
+  # cache_dir="./pythia-70m-deduped/step3000",
 )
 
-inputs = tokenizer("Hello, I am", return_tensors="pt")
-tokens = model.generate(**inputs)
+inputs = tokenizer("Hello, I am", return_tensors="pt").to(model.device)
+tokens = model.generate(**inputs,
+                        do_sample=False,
+                        max_new_tokens=50,
+                        repetition_penalty=1.0008,
+                        )
 tokenizer.decode(tokens[0])
 print(tokenizer.decode(tokens[0]))
