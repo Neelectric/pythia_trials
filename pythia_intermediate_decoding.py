bb23a3e9af592d22e2fd0111bf7bbb5ce9bfdecc
Neelectric <Neel.R@web.de>
2024-07-31 08:07:24 +0200

    Got Pythia intermediate decoding working :)

---
+++ pythia_intermediate_decoding.py
@@ -0,0 +1,114 @@
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+class AttnWrapper(torch.nn.Module):
+    def __init__(self, attn):
+        super().__init__()
+        self.attn = attn
+        self.activations = None
+        self.add_tensor = None
+
+    def forward(self, *args, **kwargs):
+        output = self.attn(*args, **kwargs)
+        if self.add_tensor is not None:
+            output = (output[0] + self.add_tensor,)+output[1:]
+        self.activations = output[0]
+        return output
+
+    def reset(self):
+        self.activations = None
+        self.add_tensor = None
+
+class BlockOutputWrapper(torch.nn.Module):
+    def __init__(self, block, unembed_matrix, norm):
+        super().__init__()
+        self.block = block
+        self.unembed_matrix = unembed_matrix
+        self.norm = norm
+
+        self.block.attention = AttnWrapper(self.block.attention)
+        self.post_attention_layernorm = self.block.post_attention_layernorm
+
+        self.attn_mech_output_unembedded = None
+        self.intermediate_res_unembedded = None
+        self.mlp_output_unembedded = None
+        self.block_output_unembedded = None
+
+
+    def forward(self, *args, **kwargs):
+        output = self.block(*args, **kwargs)
+        self.block_output_unembedded = self.unembed_matrix(self.norm(output[0]))
+        attn_output = self.block.attention.activations
+        self.attn_mech_output_unembedded = self.unembed_matrix(self.norm(attn_output))
+        attn_output += args[0]
+        self.intermediate_res_unembedded = self.unembed_matrix(self.norm(attn_output))
+        mlp_output = self.block.mlp(self.post_attention_layernorm(attn_output))
+        self.mlp_output_unembedded = self.unembed_matrix(self.norm(mlp_output))
+        return output
+
+    def attn_add_tensor(self, tensor):
+        self.block.attention.add_tensor = tensor
+
+    def reset(self):
+        self.block.attention.reset()
+
+    def get_attn_activations(self):
+        return self.block.attention.activations
+
+class PythiaHelper:
+    def __init__(self):
+        model_id = "EleutherAI/pythia-70m-deduped"
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.tokenizer = AutoTokenizer.from_pretrained(model_id)
+        self.model = AutoModelForCausalLM.from_pretrained(model_id).to(self.device)
+        for i, layer in enumerate(self.model.base_model.layers):
+            self.model.base_model.layers[i] = BlockOutputWrapper(layer, self.model.embed_out, self.model.base_model.final_layer_norm)
+
+    def generate_text(self, prompt, max_length=100):
+        inputs = self.tokenizer(prompt, return_tensors="pt")
+        generate_ids = self.model.generate(inputs.input_ids.to(self.device), max_length=max_length)
+        return self.tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
+
+    def get_logits(self, prompt):
+        inputs = self.tokenizer(prompt, return_tensors="pt")
+        with torch.no_grad():
+          logits = self.model(inputs.input_ids.to(self.device)).logits
+          return logits
+
+    def set_add_attn_output(self, layer, add_output):
+        self.model.model.layers[layer].attn_add_tensor(add_output)
+
+    def get_attn_activations(self, layer):
+        return self.model.model.layers[layer].get_attn_activations()
+
+    def reset_all(self):
+        for layer in self.model.model.layers:
+            layer.reset()
+
+    def print_decoded_activations(self, decoded_activations, label):
+        softmaxed = torch.nn.functional.softmax(decoded_activations[0][-1], dim=-1)
+        values, indices = torch.topk(softmaxed, 10)
+        probs_percent = [int(v * 100) for v in values.tolist()]
+        tokens = self.tokenizer.batch_decode(indices.unsqueeze(-1))
+        print(label, list(zip(tokens, probs_percent)))
+
+
+    def decode_all_layers(self, text, topk=2, print_attn_mech=True, print_intermediate_res=True, print_mlp=True, print_block=True):
+        self.get_logits(text)
+        for i, layer in enumerate(self.model.base_model.layers):
+            print(f'Layer {i}: Decoded intermediate outputs')
+            if print_attn_mech:
+                self.print_decoded_activations(layer.attn_mech_output_unembedded, 'Attention mechanism')
+            if print_intermediate_res:
+                self.print_decoded_activations(layer.intermediate_res_unembedded, 'Intermediate residual stream')
+            if print_mlp:
+                self.print_decoded_activations(layer.mlp_output_unembedded, 'MLP output')
+            if print_block:
+                self.print_decoded_activations(layer.block_output_unembedded, 'Block output')
+
+
+
+
+
+model = PythiaHelper()
+model.decode_all_layers('The most important political question in the world is', print_intermediate_res=False, print_mlp=False,print_block=False)
