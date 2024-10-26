e74a4d7071fa5c260e17a93aa08f9d5b7fa0f565
Neelectric <Neel.R@web.de>
2024-10-26 18:30:59 +0100

    Now able to plot for multiple equations

--- intermediate_decoding.py
+++ intermediate_decoding.py
@@ -1,6 +1,7 @@
 # system imports
 import time
 import json
+import itertools
 
 # external imports
 from transformers import GPTNeoXForCausalLM, AutoModelForCausalLM, AutoTokenizer, OlmoForCausalLM
@@ -9,6 +10,8 @@ from tqdm import tqdm
 from datasets import load_dataset
 import seaborn as sns
 import matplotlib.pyplot as plt
+from matplotlib.ticker import PercentFormatter
+
 
 # local imports
 
@@ -166,29 +169,55 @@ class IntermediateDecoder:
                 decoded_block_activations = self.return_decoded_activations(layer.block_output_unembedded, f'Block {i} output', topk)
                 if printing: print(decoded_block_activations)
                 block_activations.append(decoded_block_activations)
-            print("\n")
+            if printing: print("\n")
         return block_activations
 
 # model_id = "allenai/OLMo-7B-0724-hf"
 model_id = "EleutherAI/pythia-6.9B-deduped"
 model = IntermediateDecoder(model_id=model_id)
 
+questions = ["45+54", "29+51"]
+answers = ["99", "80"]
+all_probabilities = []
 
-question = "54+45"
-prompt = f"Question: What is {question}? Answer: {question}="
-model.reset_all()
-block_activations = model.decode_all_layers(prompt, 
-                        topk=5,
-                        printing=False,
-                        print_attn_mech=False, 
-                        print_intermediate_res=False, 
-                        print_mlp=False, 
-                        print_block=True
-                        )
-# output = model.generate_text(prompt, max_new_tokens=7,)
-# print(output)
+for question, answer in zip(questions, answers):
+    prompt = f"Question: What is {question}? Answer: {question}="
+    model.reset_all()
+    block_activations = model.decode_all_layers(prompt, 
+                            topk=5,
+                            printing=False,
+                            print_attn_mech=False, 
+                            print_intermediate_res=False, 
+                            print_mlp=False, 
+                            print_block=True
+                            )
+    block_numbers = []
+    probabilities = []
 
-for block_activation in block_activations:
-    print(block_activation)
+    for block_activation in block_activations:
+        block_num = int(block_activation[0].split()[1])
+        token_probs = dict(block_activation[1])
+        # print(block_num)
+        # print(token_probs)
+        probability_correct = token_probs.get(answer, 0)
+        block_numbers.append(block_num)
+        probabilities.append(probability_correct)
+    all_probabilities.append(probabilities)
 
+colors = itertools.cycle(sns.color_palette("tab10"))
+
+sns.set(style="whitegrid")
+plt.figure(figsize=(10, 6))
+for probabilities, answer in zip(all_probabilities, answers):
+    sns.lineplot(x=block_numbers, y=probabilities, marker='o', label=f"Probability of '{answer}'", color=next(colors))
+    print("PRINTING")
+    print(probabilities)
+    print("-"*100)
+plt.xlabel("Block Number")
+plt.ylabel("Probability (%)")
+plt.title("Probability of Tokens Across Decoder Blocks")
+plt.ylim(0, 100)
+plt.gca().yaxis.set_major_formatter(PercentFormatter())
+plt.legend()
+plt.show()
 
